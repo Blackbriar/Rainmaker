a4269ce8d9ff0ad8465359d2d22a57221187af19
Lucas <agtlucas@me.com>
2019-04-16 22:26:24 -0300

    Add basic setup

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+const rollup = require('rollup');
+const babel = require('rollup-plugin-babel');
+const pkg = require('./package.json');
+
+const config = {
+  input: './src/index.js',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+    },
+    {
+      file: pkg.module,
+      format: 'esm',
+    },
+  ],
+  plugins: [
+    babel({ exclude: './node_modules' }),
+  ],
+};
+
+export default config;
